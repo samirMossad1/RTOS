95e764d8f3766220155bfb46a9f7f8e2359ea0cf
samirMossad1 <samirmossad6@gmail.com>
2019-08-21 15:56:18 +0200

    added the implementation of the list remove function

--- RTOS/sources/rtos_list.c
+++ RTOS/sources/rtos_list.c
@@ -30,3 +30,33 @@ void RTOS_listInsert(RTOS_list_t* list_ptr,RTOS_listItem_t* new_listItem_ptr)
 
 }
 
+void RTOS_listRemove(const RTOS_listItem_t* removed_listItem_ptr)
+{
+
+	CHECK_ERROR(removed_listItem_ptr==NULL_PTR);
+
+
+	RTOS_list_t* list_ptr;
+
+	removed_listItem_ptr->nextItem_ptr->prevItem_ptr=removed_listItem_ptr->prevItem_ptr;
+
+	removed_listItem_ptr->prevItem_ptr->nextItem_ptr=removed_listItem_ptr->nextItem_ptr;
+
+    list_ptr=(RTOS_list_t*)(removed_listItem_ptr->list_ptr);
+
+    list_ptr->number_elements--;
+
+    if(list_ptr->index_ptr == removed_listItem_ptr)
+    {
+    	list_ptr->index_ptr= removed_listItem_ptr->prevItem_ptr;
+    }
+    else
+    {
+    		/* Do Nothing*/
+    }
+
+    removed_listItem_ptr->list_ptr=NULL_PTR;
+
+}
+
+
