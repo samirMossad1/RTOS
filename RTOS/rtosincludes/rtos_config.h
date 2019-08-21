2882c11228a6923491a260880e411ce67542acc3
samirMossad1 <samirmossad6@gmail.com>
2019-08-21 18:39:49 +0200

    added more comments

--- RTOS/rtosincludes/rtos_config.h
+++ RTOS/rtosincludes/rtos_config.h
@@ -60,12 +60,34 @@
 #define DOUBLE_WORD_SIZE				((uint32_t)(8U))
 
 
+
+
+/*
+ * Exception word shift related to the stack design
+ * Exception Return value
+ */
 #define EXCEPTION_WORD_SHIFT				(0X01)
 #define EXCEPTION_RETURN_PSP_UNPR_NO_FPU	((uint32_t)(0xFFFFFFFD))
 
+
+
+
+
+/*
+ * Control word shift related to the stack design
+ * Control Register value
+ */
 #define CONTROL_WORD_SHIFT					(0X10)
 #define CONTROL_REGISTER_PSP_UNPR_NO_FPU	((uint32_t)(0x03))
 
+
+
+
+
+/*
+ * Program Status Register word shift related to the stack design
+ * Program Status Register value
+ */
 #define PSRT_WORD_SHIFT						(0X11)
 #define PSR_THUMB_ACTION					((uint32_t)(0x01000000))
 
