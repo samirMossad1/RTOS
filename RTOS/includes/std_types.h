923cc2dbb40d91ba7ff313738ea7e1b8f2625d45
samirMossad1 <samirmossad6@gmail.com>
2019-08-21 13:40:07 +0200

    added the 2 need files which contain the proper type defines and the commonly used macros

---
+++ RTOS/includes/std_types.h
@@ -0,0 +1,59 @@
+/*
+ * 		Module: Standard - Types.
+ *
+ *		File Name: std_types.h
+ *
+ *		Disc: standard types
+ *
+ * 	    Created on: Feb 5, 2019
+ *
+ *      Author: Samir Mossad Ibrahim
+ */
+
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+
+
+
+#ifndef FALSE
+#define FALSE 	(0u)
+#endif
+
+#ifndef TRUE
+#define TRUE 	(1u)
+#endif
+
+#ifndef HIGH
+#define HIGH  	(1u)
+#endif
+
+
+#ifndef LOW
+#define LOW  	(0u)
+#endif
+
+
+#define NULL_PTR (void*)(0)
+
+
+typedef unsigned char bool;
+
+
+
+typedef unsigned char 		uint8_t;
+typedef signed char 		suint8_t;
+typedef unsigned short 		uint16_t;
+typedef signed short 		suint16_t;
+typedef unsigned long 		uint32_t;
+typedef signed long 		suint32_t;
+typedef unsigned long long	uint64_t;
+typedef signed long long	suint64_t;
+typedef  float 				float32_t;
+typedef  double 			float64_t;
+
+
+
+
+
+#endif /* STD_TYPES_H_ */
