5472d9ecafa244d04c178ad356cbde523e86472f
samirMossad1 <samirmossad6@gmail.com>
2019-08-22 15:24:07 +0200

    added the protoype of the svc main handler which will be branched to from the svc handler

--- RTOS/rtosincludes/rtos.h
+++ RTOS/rtosincludes/rtos.h
@@ -12,4 +12,27 @@
 #include "rtos_thread.h"
 #include "rtos_list.h"
 
+
+
+
+
+
+
+
+
+/* ------------------------------------------------------------
+ *Function-Name:RTOS_threadInitLists
+ *
+ *Description: This function is to be branched to from the SVC handler
+ *			   it will extract the SVC number
+ *			   based on this number it will call other functions
+ *
+ *Inputs:	reference to uint32_t (The process stack pointer addresse)
+ *
+ *Returns: None
+ *
+ *--------------------------------------------------------------*/
+void RTOS_SVC_Handler_main(uint32_t*);
+
+
 #endif /* RTOSINCLUDES_RTOS_H_ */
